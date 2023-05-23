01c825cc833c2ccbbf301073f629c81524713786
Tony <40310184+tonyk13@users.noreply.github.com>
2023-05-23 00:56:36 -0400

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/32408.jpg") {
+    myImage.setAttribute("src", "images/hqdefault.jpg");
+  } else {
+    myImage.setAttribute("src", "images/32408.jpg");
+  }
+};
+
+// let myButton = document.querySelector("button");
+// let myHeading = document.querySelector("h1");
+
+// function setUserName() {
+//     const myName = prompt("Please enter your name.");
+//     if (!myName) {
+//         setUserName();
+//     } else {
+//         localStorage.setItem("name", myName);
+//         myHeading.textContent = `Welcome to the OVO sweatshop, ${myName}`;
+//     }
+//   }
+  
+
+// myButton.onclick = () => {
+//     setUserName();
+// };
